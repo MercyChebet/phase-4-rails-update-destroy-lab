d9fd28f7f3f1600d80c6161c182524ffd67549f1
MercyChebet <chebetm96@gmail.com>
2022-11-30 10:27:55 +0300

    Completed Assignment

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,26 +1,37 @@
 class PlantsController < ApplicationController
 
-  # GET /plants
-  def index
-    plants = Plant.all
-    render json: plants
-  end
+ # GET /plants
+ def index
+  plants = Plant.all
+  render json: plants
+end
 
-  # GET /plants/:id
-  def show
-    plant = Plant.find_by(id: params[:id])
-    render json: plant
-  end
+# GET /plants/:id
+def show
+  plant = Plant.find_by(id: params[:id])
+  render json: plant
+end
 
-  # POST /plants
-  def create
-    plant = Plant.create(plant_params)
-    render json: plant, status: :created
-  end
+# POST /plants
+def create
+  plant = Plant.create(plant_params)
+  render json: plant, status: :created
+end
+def update
+  plant = Plant.find_by(id: params[:id])
+  plant.update(plant_params)
+  render json: plant, status: :ok
+end
 
-  private
+def destroy
+  plant = Plant.find(params[:id])
+  plant.destroy
+  render status: :no_content
+end
 
-  def plant_params
-    params.permit(:name, :image, :price, :is_in_stock)
-  end
+private
+
+def plant_params
+  params.permit(:name, :image, :price, :is_in_stock)
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,2 @@
 Rails.application.routes.draw do
-  resources :plants, only: [:index, :show, :create]
-end
+  resources :plants
